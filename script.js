df745bf163466bead446bd03529666dc6bf6fcdf
albatrosiks <93161110+albatrosiks@users.noreply.github.com>
2022-06-07 21:22:31 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+const button = document.getElementById('button');
+const audioElement = document.getElementById('audio');
+
+// Disable/Enable Button
+function toggleButton() {
+    button.disabled = !button.disabled;
+}
+
+// Passing Joke to VoiceRSS API
+function tellMe(joke) {
+    VoiceRSS.speech({
+        key: '50f343aa53e84e89a9d9330bd9d76387',
+        // key should be hidden away when we use the paid version
+        src: joke,
+        hl: 'en-us',
+        v: 'Linda',
+        r: 0,
+        c: 'mp3',
+        f: '44khz_16bit_stereo',
+        ssml: false
+    });
+}
+
+// Get Jokes from Jokes API
+async function getJokes() {
+    let joke = '';
+    const apiUrl = 'https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single';
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        if (data.setup) {
+            joke = `${data.setup} ... ${data.delivery}`;
+        } else {
+            joke = data.joke;
+        }
+        // Text-to-Speech
+        tellMe(joke);
+        // Disable button
+        toggleButton();
+    } catch (error) {
+        // catch errors 
+        console.log('whoops', error);
+    }
+}
+
+// Event Listeners
+button.addEventListener('click', getJokes);
+audioElement.addEventListener('ended', toggleButton);
